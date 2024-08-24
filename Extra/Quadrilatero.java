3cc0076baab35ce9787e17c278a4e93abbcdc062
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-24 15:45:32 -0300

    Create Quadrilatero.java

---
+++ Extra/Quadrilatero.java
@@ -0,0 +1,8 @@
+/*
+Escreva uma hierarquia de herança para as classes Quadrilátero, Trapézio, Paralelogramo, Retângulo e Quadrado. 
+Utilize Quadrilátero como a superclasse da hierarquia. 
+Faça a hierarquia o mais profunda (isto é, com muitos níveis) possível. 
+Especifique as variáveis de instância e os métodos para cada classe. 
+As variáveis de instância private de Quadrilátero devem ser os pares de coordenadas x−y para os quatro pontos que delimitam o Quadrilátero. 
+Escreva um programa que instancia objetos de suas classes e gera saída da área de cada objeto (exceto Quadrilátero).
+*/
